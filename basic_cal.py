a87b34450694f1639eff73534b43f0281d7414d7
peter emeka <peteremeka51@gmail.com>
2025-08-12 13:54:22 +0100

    basic calculation with python

---
+++ basic_cal.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+#ADDITION
+print ("sum: ", num1 + num2)
+
+#MULTIPLICATION
+print ("product: ", num1 * num2)
+
+#SUBTRACTION
+print ("product: ", num1 - num2)
+
+#division
+print ("product: ", num1 / num2)
